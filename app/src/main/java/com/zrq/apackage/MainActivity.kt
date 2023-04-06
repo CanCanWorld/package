8db820d59b152336ea519fc0af925c1535f85eb8
zrq <3480197195@qq.com>
2023-04-06 18:16:23 +0800

    init

--- app/src/main/java/com/zrq/apackage/MainActivity.kt
+++ app/src/main/java/com/zrq/apackage/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.zrq.apackage
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.zrq.apackage.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+        requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+
+        initEvent()
+
+        val packages = packageManager.getInstalledPackages(0)
+        packages.forEach {
+            Log.d(TAG, "onCreate: ${it.packageName}")
+            val file = File(packageManager.getApplicationInfo(it.packageName, 0).sourceDir)
+            Log.d(TAG, "onCreate: ${file.absoluteFile}")
+        }
+    }
+
+    private fun initEvent() {
+        mBinding.recyclerView.adapter
+    }
+
+    private companion object {
+        const val TAG = "MainActivity"
     }
 }
